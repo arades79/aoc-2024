f727ac3e861a9d299f6800c8b692435843c6d27f
arades79 <contact@arades.dev>
2024-12-02 21:23:10 -0500

    solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -6,25 +6,44 @@ fn line(input: &mut &str) -> PResult<Vec<u32>> {
     separated(0.., dec_uint::<_, u32, _>, ' ').parse_next(input)
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    fn is_safe(row: &[u32]) -> bool {
-        const MAX_DIST: u32 = 3;
-        let dir = if row.len() >= 2 {
-            row[0] > row[1]
-        } else {
-            false
-        };
-        for pair in row.windows(2) {
-            if (pair[0] > pair[1]) != dir
-                || pair[0].abs_diff(pair[1]) > MAX_DIST
-                || pair[0] == pair[1]
-            {
-                return false;
-            }
+fn is_safe(row: &[u32]) -> bool {
+    const MAX_DIST: u32 = 3;
+    let dir = if row.len() >= 2 {
+        row[0] > row[1]
+    } else {
+        false
+    };
+    for pair in row.windows(2) {
+        if (pair[0] > pair[1]) != dir || pair[0].abs_diff(pair[1]) > MAX_DIST || pair[0] == pair[1]
+        {
+            return false;
         }
-        true
     }
+    true
+}
 
+fn is_safe_with_dampen(row: &[u32]) -> bool {
+    const MAX_DIST: u32 = 3;
+    let dir = if row.len() >= 2 {
+        row[0] > row[1]
+    } else {
+        false
+    };
+    for pair in row.windows(2) {
+        if (pair[0] > pair[1]) != dir || pair[0].abs_diff(pair[1]) > MAX_DIST || pair[0] == pair[1]
+        {
+            for i in 0..row.len() {
+                let mut shortened = row.to_vec();
+                shortened.remove(i);
+                if is_safe(&shortened) {return true}
+            }
+            return false
+        }
+    }
+    true
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
     let safe_lines = input
         .lines()
         .map(|s| line.parse(s).unwrap())
@@ -33,7 +52,11 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let safe_lines = input
+        .lines()
+        .map(|s| line.parse(s).unwrap())
+        .fold(0, |sum, row| sum + is_safe_with_dampen(&row) as u32);
+    Some(safe_lines)
 }
 
 #[cfg(test)]
@@ -49,6 +72,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
