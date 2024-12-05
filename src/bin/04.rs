1de1a46d7356c7e58df339a87493e22e399af6bc
arades79 <contact@arades.dev>
2024-12-05 06:53:06 -0500

    format

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -77,7 +77,10 @@ pub fn part_one(input: &str) -> Option<u32> {
     for (y, l) in mat.iter().enumerate() {
         for (x, c) in l.iter().enumerate() {
             if *c == b'X' {
-                let mut new_words = Word::candidates_from(x as u32, y as u32).into_iter().filter(|w| w.valid(&mat)).collect();
+                let mut new_words = Word::candidates_from(x as u32, y as u32)
+                    .into_iter()
+                    .filter(|w| w.valid(&mat))
+                    .collect();
                 words.append(&mut new_words);
             };
         }
@@ -88,20 +91,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 struct Cross(u32, u32);
 
 impl Cross {
-    fn from_mat(x:u32, y:u32, mat: &[&[u8]]) -> Option<Cross> {
-        let get = |xoff, yoff| mat.get(y.checked_add_signed(yoff)?  as usize)?.get(x.checked_add_signed(xoff)? as usize);
-        if mat[y as usize][x as usize] != b'A' {return None};
+    fn from_mat(x: u32, y: u32, mat: &[&[u8]]) -> Option<Cross> {
+        let get = |xoff, yoff| {
+            mat.get(y.checked_add_signed(yoff)? as usize)?
+                .get(x.checked_add_signed(xoff)? as usize)
+        };
+        if mat[y as usize][x as usize] != b'A' {
+            return None;
+        };
         match get(-1, -1)? {
-            b'M' => if *get(1,1)? != b'S' {return None},
-            b'S' => if *get(1,1)? != b'M' {return None},
+            b'M' => {
+                if *get(1, 1)? != b'S' {
+                    return None;
+                }
+            }
+            b'S' => {
+                if *get(1, 1)? != b'M' {
+                    return None;
+                }
+            }
             _ => return None,
         };
-        match get(1,-1)? {
-            b'M' => if *get(-1,1)? != b'S' {return None},
-            b'S' => if *get(-1, 1)? != b'M' {return None},
+        match get(1, -1)? {
+            b'M' => {
+                if *get(-1, 1)? != b'S' {
+                    return None;
+                }
+            }
+            b'S' => {
+                if *get(-1, 1)? != b'M' {
+                    return None;
+                }
+            }
             _ => return None,
         };
-        return Some(Cross(x,y))
+        return Some(Cross(x, y));
     }
 }
 
@@ -114,7 +138,7 @@ pub fn part_two(input: &str) -> Option<u32> {
                 crosses.push(cross)
             }
         }
-    }    
+    }
     Some(crosses.len() as u32)
 }
 
