5fe5936b2a7538a30fe56a636032daf728a7f2e4
arades79 <contact@arades.dev>
2024-12-06 10:59:02 -0500

    solve day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,201 @@
+use std::{collections::HashSet, ops::Deref};
+
+use itertools::Itertools;
+
+advent_of_code::solution!(6);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+impl Direction {
+    fn rotate(self) -> Self {
+        match self {
+            Direction::Left => Direction::Up,
+            Direction::Right => Direction::Down,
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Empty(bool),
+    Obstacle,
+    Guard(Direction),
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        Tile::Empty(false)
+    }
+}
+
+#[derive(Debug,Clone, Copy,PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Guard {
+    position: (usize, usize),
+    direction: Direction,
+}
+
+impl Guard {
+    fn next_pos(&self) -> Option<(usize, usize)> {
+        let (x, y) = self.position;
+        match self.direction {
+            Direction::Left => Some((x.checked_sub(1)?, y)),
+            Direction::Right => Some((x + 1, y)),
+            Direction::Down => Some((x, y + 1)),
+            Direction::Up => Some((x, y.checked_sub(1)?)),
+        }
+    }
+    fn previous_pos(&self) -> Option<(usize, usize)> {
+        let (x, y) = self.position;
+        match self.direction {
+            Direction::Right => Some((x.checked_sub(1)?, y)),
+            Direction::Left => Some((x + 1, y)),
+            Direction::Up => Some((x, y + 1)),
+            Direction::Down => Some((x, y.checked_sub(1)?)),
+        }
+    }
+    fn rotate(&mut self) {
+        self.direction = self.direction.rotate()
+    }
+}
+
+fn parse_tile(c: char) -> Tile {
+    match c {
+        '.' => Tile::Empty(false),
+        '#' => Tile::Obstacle,
+        'v' => Tile::Guard(Direction::Down),
+        '^' => Tile::Guard(Direction::Up),
+        '<' => Tile::Guard(Direction::Left),
+        '>' => Tile::Guard(Direction::Right),
+        _ => panic!("input shouldn't have this character!"),
+    }
+}
+
+type Map = Box<[Box<[Tile]>]>;
+
+fn parse_map(input: &str) -> Map {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(parse_tile)
+                .collect::<Vec<_>>()
+                .into_boxed_slice()
+        })
+        .collect::<Vec<_>>()
+        .into_boxed_slice()
+}
+
+fn find_guard(map: &Map) -> Option<Guard> {
+    for (y, tiles) in map.iter().enumerate() {
+        for (x, tile) in tiles.iter().enumerate() {
+            if let Tile::Guard(dir) = tile {
+                return Some(Guard {
+                    position: (x, y),
+                    direction: *dir,
+                });
+            }
+        }
+    }
+    None
+}
+
+fn iterate_map(map: &mut Map, mut guard: Guard) -> Option<Guard> {
+    map[guard.position.1][guard.position.0] = Tile::Empty(true);
+    let (mut x, mut y) = guard.next_pos()?;
+    loop {
+        match *map.get(y)?.get(x)? {
+            Tile::Empty(_) => {
+                map[y][x] = Tile::Guard(guard.direction);
+                break;
+            }
+            Tile::Obstacle => {
+                guard.rotate();
+                (x, y) = guard.next_pos()?;
+            }
+            Tile::Guard(_) => panic!("*spiderman pointing at spiderman*"),
+        }
+    }
+    Some(Guard {
+        position: (x, y),
+        direction: guard.direction,
+    })
+}
+
+fn detect_loop(map: &mut Map, mut guard: Guard) -> bool {
+    let mut guard_states = HashSet::new();
+    guard_states.insert(guard);
+    while let Some(new_guard) = iterate_map(map, guard) {
+        if guard_states.contains(&new_guard) {return true}
+        guard = new_guard;
+        guard_states.insert(guard);
+    }
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut map = parse_map(input);
+    let mut guard = find_guard(&map)?;
+    while let Some(new_guard) = iterate_map(&mut map, guard) {
+        guard = new_guard
+    }
+    let traversed = map.iter().flatten().fold(0, |traversed, t| {
+        if *t == Tile::Empty(true) {
+            traversed + 1
+        } else {
+            traversed
+        }
+    });
+    let tiles = map[0].len() * map.len();
+    println!("guard traversed {traversed} out of {tiles} tiles");
+    Some(traversed)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let initial_map = parse_map(input);
+    let initial_guard = find_guard(&initial_map)?;
+    let traversed_tiles = {
+        let mut map = initial_map.clone();
+        let mut guard = initial_guard;
+        let mut traverse_locations = HashSet::new();
+        while let Some(new_guard) = iterate_map(&mut map, guard) {
+            guard = new_guard;
+            traverse_locations.insert(guard.position);
+        }
+        traverse_locations
+    };
+    let mut loops = 0;
+    for traversed in traversed_tiles {
+        let mut map = initial_map.clone();
+        map[traversed.1][traversed.0] = Tile::Obstacle;
+        if detect_loop(&mut map, initial_guard) {
+        loops += 1;
+        println!("loop created by placing obstical at {traversed:?}");
+        }
+    }
+    Some(loops)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(41));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+}
