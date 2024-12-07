259dff4ea23082fe41aedd505a026e91ed799602
arades79 <contact@arades.dev>
2024-12-06 22:56:06 -0500

    make things const (no speedup)

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,7 +2,6 @@ use std::collections::HashSet;
 
 use tokio::task::JoinSet;
 
-
 advent_of_code::solution!(6);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -14,7 +13,7 @@ enum Direction {
 }
 
 impl Direction {
-    fn rotate(self) -> Self {
+    const fn rotate(self) -> Self {
         match self {
             Direction::Left => Direction::Up,
             Direction::Right => Direction::Down,
@@ -44,21 +43,33 @@ struct Guard {
 }
 
 impl Guard {
-    fn next_pos(&self) -> Option<(usize, usize)> {
+    const fn next_pos(&self) -> Option<(usize, usize)> {
         let (x, y) = self.position;
         match self.direction {
-            Direction::Left => Some((x.checked_sub(1)?, y)),
+            Direction::Left => Some((
+                match x.checked_sub(1) {
+                    Some(x) => x,
+                    None => return None,
+                },
+                y,
+            )),
             Direction::Right => Some((x + 1, y)),
             Direction::Down => Some((x, y + 1)),
-            Direction::Up => Some((x, y.checked_sub(1)?)),
+            Direction::Up => Some((
+                x,
+                match y.checked_sub(1) {
+                    Some(y) => y,
+                    None => return None,
+                },
+            )),
         }
     }
-    fn rotate(&mut self) {
+    const fn rotate(&mut self) {
         self.direction = self.direction.rotate()
     }
 }
 
-fn parse_tile(c: char) -> Tile {
+const fn parse_tile(c: char) -> Tile {
     match c {
         '.' => Tile::Empty(false),
         '#' => Tile::Obstacle,
@@ -99,18 +110,25 @@ fn find_guard(map: &Map) -> Option<Guard> {
     None
 }
 
-fn iterate_map(map: &mut Map, mut guard: Guard) -> Option<Guard> {
+const fn iterate_map(map: &mut Map, mut guard: Guard) -> Option<Guard> {
     map[guard.position.1][guard.position.0] = Tile::Empty(true);
-    let (mut x, mut y) = guard.next_pos()?;
+    let (mut x, mut y) = match guard.next_pos() {
+        Some((x, y)) => (x,y),
+        None => return None,
+    };
     loop {
-        match *map.get(y)?.get(x)? {
+        if y >= map.len() || x >= map[0].len() {return None};
+        match map[y][x] {
             Tile::Empty(_) => {
                 map[y][x] = Tile::Guard(guard.direction);
                 break;
             }
             Tile::Obstacle => {
                 guard.rotate();
-                (x, y) = guard.next_pos()?;
+                (x, y) = match guard.next_pos() {
+                    Some((x, y)) => (x,y),
+                    None => return None,
+                };
             }
             Tile::Guard(_) => panic!("*spiderman pointing at spiderman*"),
         }
@@ -158,22 +176,22 @@ pub async fn part_two(input: &str) -> Option<u32> {
     let mut obstructed_locations = HashSet::new();
     let mut guard_states = HashSet::new();
     let mut tasks = JoinSet::new();
-        while let Some(new_guard) = iterate_map(&mut map, guard) {
-            guard_states.insert(guard);
-            if !obstructed_locations.contains(&new_guard.position) {
-                {
-                    let alt_guard_states = guard_states.clone();
-                    let mut alt_map = map.clone();
-                    alt_map[guard.position.1][guard.position.0] = Tile::Guard(guard.direction);
-                    alt_map[new_guard.position.1][new_guard.position.0] = Tile::Obstacle;
-                    tasks.spawn_blocking(move || {
-                        detect_loop(&mut alt_map, guard, alt_guard_states) as u32
-                    });
-                }
-                obstructed_locations.insert(new_guard.position);
+    while let Some(new_guard) = iterate_map(&mut map, guard) {
+        guard_states.insert(guard);
+        if !obstructed_locations.contains(&new_guard.position) {
+            {
+                let alt_guard_states = guard_states.clone();
+                let mut alt_map = map.clone();
+                alt_map[guard.position.1][guard.position.0] = Tile::Guard(guard.direction);
+                alt_map[new_guard.position.1][new_guard.position.0] = Tile::Obstacle;
+                tasks.spawn_blocking(move || {
+                    detect_loop(&mut alt_map, guard, alt_guard_states) as u32
+                });
             }
-            guard = new_guard;
+            obstructed_locations.insert(new_guard.position);
         }
+        guard = new_guard;
+    }
     let loops = tasks.join_all().await.into_iter().sum();
     Some(loops)
 }
